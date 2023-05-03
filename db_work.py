e25b8c6fddfd80647bfbaddd4355fe015445d7cd
Vlodeeek <58884141+VovaMishchenia23@users.noreply.github.com>
2023-05-03 14:05:13 +0300

    add commnets

--- db_work.py
+++ db_work.py
@@ -8,32 +8,44 @@ from get_prediction_db import get_prediction
 DATA_FOLDER = "data/"
 LAST_TIME_PREDICTION_FILE = "last_time_prediction.pkl"
 
-
+# insert predictions for all regions in db
 def insert_data_db(connection, engine):
+    # writing logs
     with open("logger.txt", "a") as file:
         file.write(f"{datetime.datetime.now().strftime('%H:%M:%S')}: Inserting in db...\n")
     try:
+        # inserting data to db
         metadata = db.MetaData()
         predictions_table = db.Table('predictions', metadata, autoload_with=engine)
-
+        
+        # get predictions
         values = get_prediction()
+        
+        # load the datetime of inserting new prediction data to db
         with open(f"{DATA_FOLDER}{LAST_TIME_PREDICTION_FILE}", "wb") as file:
             pickle.dump(datetime.datetime.now(), file)
+        
+        # delete old data
         query = db.delete(predictions_table)
         result_proxy = connection.execute(query)
         print("Deleting data: ", result_proxy)
+        
+        # insert new data
         query = db.insert(predictions_table)
         result_proxy = connection.execute(query, values)
         print("Inserting new data: ", result_proxy)
+        
+        # commit changes in db
         connection.commit()
         with open("logger.txt", "a") as file:
             file.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: New data was inserted successfully\n")
+            
     except sqlalchemy.exc.OperationalError:
         with open("logger.txt", "a") as file:
             file.write(
                 f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: Fail inserting new data in db: too many connections fo role\n")
 
-
+# get data from db for specific region by region_id or for all regions if region_id=None
 def get_data(connection, engine, region_id=None):
     metadata = db.MetaData()
     predictions_table = db.Table('predictions', metadata, autoload_with=engine)
