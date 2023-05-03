53832dfcdf0feaee79d14381cab805116fe48a69
Vlodeeek <58884141+VovaMishchenia23@users.noreply.github.com>
2023-05-03 13:58:28 +0300

    add comments

--- get_prediction_db.py
+++ get_prediction_db.py
@@ -7,12 +7,15 @@ import pandas as pd
 
 MODEL_FOLDER = "models/"
 DATA_FOLDER = "data/"
+
+# top 3 models for prediction alerts
 SGD = "11__sgd__v2.pkl"
 LOGISTIC_REGRESSION = "11__logistic_regression__v3.pkl"
 SGD_CLASSIFIER = "11__sgd_classifier__v2.pkl"
+
 model_path = f"{MODEL_FOLDER}{SGD_CLASSIFIER}"
 
-
+# create dictionary region name-region id for binding data
 def get_regions_id():
     region_df = pd.read_csv(f"{DATA_FOLDER}regions.csv")
     locations = list(region_df["center_city_en"].values)
@@ -23,7 +26,7 @@ def get_regions_id():
         res[region] = region_df.loc[region_df['center_city_en'] == region]['region_id'].iloc[0]
     return res
 
-
+# create dictionary region id-region name for binding data
 def get_id_region():
     region_df = pd.read_csv(f"{DATA_FOLDER}regions.csv")
     locations = list(region_df["center_city_en"].values)
@@ -36,13 +39,18 @@ def get_id_region():
 
 
 def get_prediction():
+    # load ml model for prediction
     with open(model_path, "rb") as file:
         model = pickle.load(file)
 
+    # get matrix for prediction
     matrix = get_matrix()
+    
+    # predict alerts
     predicted = list(model.predict(matrix))
     # print(predicted)
 
+    # formating data for adding to the database
     response = []
     i = 0
     j = 0
